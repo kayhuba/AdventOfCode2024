f864418ad96778e9d81c79ab01cffc271a1185cd
Kay Huber <kay.huber@unblu.com>
2024-12-01 10:20:45 +0100

    Day01 puzzle2 solution

--- day01/build/puzzle02.js
+++ day01/build/puzzle02.js
@@ -5,6 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 console.log("Day 01, Puzzle 02!");
 const line_reader_1 = __importDefault(require("line-reader"));
+const listOne = [];
+const listTwo = [];
 line_reader_1.default.eachLine("./input/input.txt", (line, last) => {
+    const matches = /^(\d+)\s+(\d+)$/.exec(line);
+    if (matches !== null) {
+        listOne.push(parseInt(matches[1]));
+        listTwo.push(parseInt(matches[2]));
+    }
+    if (last) {
+        const sortedListOne = listOne.sort();
+        const sortedListTwo = listTwo.sort();
+        const occurrenceMap = new Map();
+        sortedListTwo.forEach(locationId => {
+            const occurrence = occurrenceMap.get(locationId) || 0;
+            occurrenceMap.set(locationId, occurrence + 1);
+        });
+        const totalSimilarityScore = sortedListOne.reduce((similarityScore, locationId) => similarityScore + (locationId * (occurrenceMap.get(locationId) || 0)), 0);
+        console.log(`Similarity score: ${totalSimilarityScore}`);
+    }
 });
 //# sourceMappingURL=puzzle02.js.map
--- day01/src/puzzle02.ts
+++ day01/src/puzzle02.ts
@@ -2,5 +2,26 @@ console.log("Day 01, Puzzle 02!")
 
 import lineReader from "line-reader";
 
+const listOne: number[] = [];
+const listTwo: number[] = [];
 lineReader.eachLine("./input/input.txt", (line, last) => {
+    const matches = /^(\d+)\s+(\d+)$/.exec(line);
+    if (matches !== null) {
+        listOne.push(parseInt(matches[1]));
+        listTwo.push(parseInt(matches[2]));
+    }
+
+    if (last) {
+        const sortedListOne = listOne.sort();
+        const sortedListTwo = listTwo.sort();
+
+        const occurrenceMap = new Map<number, number>();
+        sortedListTwo.forEach(locationId => {
+            const occurrence = occurrenceMap.get(locationId) || 0;
+            occurrenceMap.set(locationId, occurrence + 1);
+        });
+
+        const totalSimilarityScore = sortedListOne.reduce((similarityScore, locationId) => similarityScore + (locationId * (occurrenceMap.get(locationId) || 0)), 0);
+        console.log(`Similarity score: ${totalSimilarityScore}`);
+    }
 });
