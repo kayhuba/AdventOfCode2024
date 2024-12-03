72ee71624aa666fbbf8840133663dd226743b4e5
Kay Huber <kay.huber@unblu.com>
2024-12-03 07:09:52 +0100

    Day03 puzzle01 solution

--- day03/build/puzzle01.js
+++ day03/build/puzzle01.js
@@ -1,5 +1,20 @@
-console.log("Day 01, Puzzle 01!");
-import lineReader from "line-reader";
-lineReader.eachLine("./input/input.txt", (line, last) => {
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Day 03, Puzzle 01!");
+const line_reader_1 = __importDefault(require("line-reader"));
+let sum = 0;
+line_reader_1.default.eachLine("./input/input.txt", (line, last) => {
+    const regex = /mul\((\d{1,3},\d{1,3})\)/g;
+    let match;
+    while ((match = regex.exec(line)) !== null) {
+        const [a, b] = match[1].split(",").map(raw => parseInt(raw));
+        sum += a * b;
+    }
+    if (last) {
+        console.log(`Added up results of multiplications: ${sum}`);
+    }
 });
 //# sourceMappingURL=puzzle01.js.map
--- day03/src/puzzle01.ts
+++ day03/src/puzzle01.ts
@@ -1,6 +1,18 @@
-console.log("Day 01, Puzzle 01!")
+console.log("Day 03, Puzzle 01!")
 
 import lineReader from "line-reader";
 
+let sum = 0;
 lineReader.eachLine("./input/input.txt", (line, last) => {
+    const regex =  /mul\((\d{1,3},\d{1,3})\)/g;
+    let match;
+
+    while ((match = regex.exec(line)) !== null) {
+        const [a, b] = match[1].split(",").map(raw => parseInt(raw));
+        sum += a * b;
+    }
+
+    if (last) {
+        console.log(`Added up results of multiplications: ${sum}`)
+    }
 });
