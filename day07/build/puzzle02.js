4a897f7f3145b25014ede5d35d02c72f9525d7c5
Kay Huber <kay.huber@unblu.com>
2024-12-07 11:59:06 +0100

    Day07 puzzle02 solution

--- day07/build/puzzle02.js
+++ day07/build/puzzle02.js
@@ -3,8 +3,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Day 01, Puzzle 02!");
+console.log("Day 07, Puzzle 02!");
 const line_reader_1 = __importDefault(require("line-reader"));
+const Add = {
+    evaluate: (a, b) => a + b
+};
+const Mul = {
+    evaluate: (a, b) => a * b
+};
+const Merge = {
+    evaluate: (a, b) => parseInt(`${a}${b}`)
+};
+let totalCalibrationResult = 0;
+const operators = [Add, Mul, Merge];
 line_reader_1.default.eachLine("./input/input.txt", (line, last) => {
+    const resultAndParameters = line.split(": ");
+    if (resultAndParameters.length > 0) {
+        const desiredResult = parseInt(resultAndParameters[0]);
+        const parameters = resultAndParameters[1].split(" ").map((parameter) => parseInt(parameter));
+        const operatorCount = parameters.length - 1;
+        const combinations = Math.pow(operators.length, operatorCount);
+        for (let i = 0; i < combinations; i++) {
+            const result = parameters.slice(1).reduce((acc, parameter, index) => {
+                const operatorIndex = Math.floor(i / Math.pow(operators.length, operatorCount - index - 1)) % operators.length;
+                return operators[operatorIndex].evaluate(acc, parameter);
+            }, parameters[0]);
+            // TODO if we order the operators in a clever way, we can abort as soon as the result is bigger than desiredResult
+            if (result === desiredResult) {
+                totalCalibrationResult += desiredResult;
+                break;
+            }
+        }
+    }
+    if (last) {
+        console.log(`Total calibration result: ${totalCalibrationResult}`);
+    }
 });
 //# sourceMappingURL=puzzle02.js.map
--- day07/src/puzzle02.ts
+++ day07/src/puzzle02.ts
@@ -1,6 +1,48 @@
-console.log("Day 01, Puzzle 02!")
+console.log("Day 07, Puzzle 02!")
 
 import lineReader from "line-reader";
 
+interface Operator {
+    evaluate: (a: number, b: number) => number;
+}
+
+const Add: Operator = {
+    evaluate: (a: number, b: number) => a + b
+}
+
+const Mul: Operator = {
+    evaluate: (a: number, b: number) => a * b
+}
+
+const Merge: Operator = {
+    evaluate: (a: number, b: number) => parseInt(`${a}${b}`)
+}
+
+let totalCalibrationResult = 0;
+const operators: Operator[] = [Add, Mul, Merge];
 lineReader.eachLine("./input/input.txt", (line, last) => {
+    const resultAndParameters = line.split(": ");
+    if (resultAndParameters.length > 0) {
+        const desiredResult = parseInt(resultAndParameters[0]);
+        const parameters = resultAndParameters[1].split(" ").map((parameter) => parseInt(parameter));
+
+        const operatorCount = parameters.length - 1;
+        const combinations = Math.pow(operators.length, operatorCount);
+        for (let i=0; i < combinations; i++) {
+            const result = parameters.slice(1).reduce((acc, parameter, index) => {
+                const operatorIndex = Math.floor(i / Math.pow(operators.length,operatorCount - index - 1)) % operators.length;
+                return operators[operatorIndex].evaluate(acc, parameter);
+            }, parameters[0]);
+
+            // TODO if we order the operators in a clever way, we can abort as soon as the result is bigger than desiredResult
+            if (result === desiredResult) {
+                totalCalibrationResult += desiredResult;
+                break;
+            }
+        }
+    }
+
+    if (last) {
+        console.log(`Total calibration result: ${totalCalibrationResult}`);
+    }
 });
