9b42eded8606a555c8c922f875b86c3c05a33837
Kay Huber <kay.huber@unblu.com>
2024-12-08 09:34:17 +0100

    Day08 puzzle01 solution

--- day08/build/puzzle01.js
+++ day08/build/puzzle01.js
@@ -1,5 +1,51 @@
-console.log("Day 01, Puzzle 01!");
-import lineReader from "line-reader";
-lineReader.eachLine("./input/input.txt", (line, last) => {
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Day 08, Puzzle 01!");
+const line_reader_1 = __importDefault(require("line-reader"));
+function isValidAntinode(antinode, width, height) {
+    return antinode.x >= 0 && antinode.x < width && antinode.y >= 0 && antinode.y < height;
+}
+function findAndApplyAntinodes(antennaPair, antinodeLocations, width, height) {
+    antennaPair.sort((a, b) => a.y - b.y || a.x - b.x);
+    const firstAntinode = {
+        x: antennaPair[0].x - (antennaPair[1].x - antennaPair[0].x),
+        y: antennaPair[0].y - (antennaPair[1].y - antennaPair[0].y)
+    };
+    if (isValidAntinode(firstAntinode, width, height)) {
+        antinodeLocations.add(`${firstAntinode.x},${firstAntinode.y}`);
+    }
+    const secondAntinode = {
+        x: antennaPair[1].x - (antennaPair[0].x - antennaPair[1].x),
+        y: antennaPair[1].y - (antennaPair[0].y - antennaPair[1].y)
+    };
+    if (isValidAntinode(secondAntinode, width, height)) {
+        antinodeLocations.add(`${secondAntinode.x},${secondAntinode.y}`);
+    }
+}
+let lineCount = 0;
+const antennas = new Map();
+line_reader_1.default.eachLine("./input/input.txt", (line, last) => {
+    line.split("").forEach((potentialAntenna, index) => {
+        if (potentialAntenna === ".") {
+            return;
+        }
+        const position = { x: index, y: lineCount };
+        if (!antennas.has(potentialAntenna)) {
+            antennas.set(potentialAntenna, []);
+        }
+        antennas.get(potentialAntenna).push(position);
+    });
+    lineCount++;
+    if (last) {
+        const antinodeLocations = new Set();
+        antennas.forEach((antennaPositions, antennaName) => {
+            const antennaPairs = antennaPositions.map((antennaPosition, index) => [...antennaPositions.slice(0, index), ...antennaPositions.slice(index + 1)].map((otherAntennaPosition) => [antennaPosition, otherAntennaPosition])).flat();
+            antennaPairs.forEach(antennaPair => findAndApplyAntinodes(antennaPair, antinodeLocations, line.length, lineCount));
+        });
+        console.log(`Unique locations of antinodes: ${antinodeLocations.size}`);
+    }
 });
 //# sourceMappingURL=puzzle01.js.map
--- day08/src/puzzle01.ts
+++ day08/src/puzzle01.ts
@@ -1,6 +1,66 @@
-console.log("Day 01, Puzzle 01!")
+console.log("Day 08, Puzzle 01!")
 
 import lineReader from "line-reader";
 
+interface Position {
+    x: number;
+    y: number;
+}
+
+function isValidAntinode(antinode: Position, width: number, height: number): boolean {
+    return antinode.x >= 0 && antinode.x < width && antinode.y >= 0 && antinode.y < height;
+}
+
+function findAndApplyAntinodes(antennaPair: [Position, Position], antinodeLocations: Set<string>, width: number, height: number): void {
+    antennaPair.sort((a, b) => a.y - b.y || a.x - b.x);
+
+    const firstAntinode: Position = {
+        x: antennaPair[0].x - (antennaPair[1].x - antennaPair[0].x),
+        y: antennaPair[0].y - (antennaPair[1].y - antennaPair[0].y)
+    };
+
+    if (isValidAntinode(firstAntinode, width, height)) {
+        antinodeLocations.add(`${firstAntinode.x},${firstAntinode.y}`);
+    }
+
+    const secondAntinode: Position = {
+        x: antennaPair[1].x - (antennaPair[0].x - antennaPair[1].x),
+        y: antennaPair[1].y - (antennaPair[0].y - antennaPair[1].y)
+    };
+
+    if (isValidAntinode(secondAntinode, width, height)) {
+        antinodeLocations.add(`${secondAntinode.x},${secondAntinode.y}`);
+    }
+}
+
+let lineCount = 0;
+const antennas: Map<string, Position[]> = new Map();
 lineReader.eachLine("./input/input.txt", (line, last) => {
+    line.split("").forEach((potentialAntenna, index) => {
+        if (potentialAntenna === ".") {
+            return;
+        }
+
+        const position: Position = { x: index, y: lineCount };
+        if (!antennas.has(potentialAntenna)) {
+            antennas.set(potentialAntenna, []);
+        }
+
+        antennas.get(potentialAntenna)!.push(position);
+    });
+
+    lineCount++;
+    if (last) {
+        const antinodeLocations: Set<string> = new Set();
+
+        antennas.forEach((antennaPositions, antennaName) => {
+            const antennaPairs = antennaPositions.map((antennaPosition, index) =>
+                [...antennaPositions.slice(0, index), ...antennaPositions.slice(index + 1)].map((otherAntennaPosition) => [antennaPosition, otherAntennaPosition])
+            ).flat();
+
+            antennaPairs.forEach(antennaPair => findAndApplyAntinodes(antennaPair as [Position, Position], antinodeLocations, line.length, lineCount));
+        });
+
+        console.log(`Unique locations of antinodes: ${antinodeLocations.size}`);
+    }
 });
