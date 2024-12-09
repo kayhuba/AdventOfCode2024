4c80838c1349b19adff04f9ffa5d456354df7d18
Kay Huber <kay.huber@unblu.com>
2024-12-09 14:14:17 +0100

    Add timer to template

--- day_template/src/puzzle01.ts
+++ day_template/src/puzzle01.ts
@@ -2,5 +2,10 @@ console.log("Day 01, Puzzle 01!")
 
 import lineReader from "line-reader";
 
+const timer = "Execution time";
+console.time(timer);
 lineReader.eachLine("./input/input.txt", (line, last) => {
+    if (last) {
+        console.timeEnd(timer);
+    }
 });
