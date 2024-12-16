1894ecbcd682f863b5b22313e4e32339cde113ef
Kay Huber <kay.huber@unblu.com>
2024-12-16 06:44:57 +0100

    Day16 start

---
+++ day16/build/puzzle01.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Day 16, Puzzle 01!");
+const line_reader_1 = __importDefault(require("line-reader"));
+const timer = "Execution time";
+console.time(timer);
+line_reader_1.default.eachLine("./input/input.txt", (line, last) => {
+    if (last) {
+        console.timeEnd(timer);
+    }
+});
+//# sourceMappingURL=puzzle01.js.map
