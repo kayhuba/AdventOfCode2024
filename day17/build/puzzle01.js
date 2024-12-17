effcc8351bd35cd8f7e92c598b03bc7f29ea0e7f
Kay Huber <kay.huber@unblu.com>
2024-12-17 09:49:08 +0100

    Day17 puzzle01 solution

--- day17/build/puzzle01.js
+++ day17/build/puzzle01.js
@@ -5,10 +5,146 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 console.log("Day 17, Puzzle 01!");
 const line_reader_1 = __importDefault(require("line-reader"));
+var OpCode;
+(function (OpCode) {
+    OpCode[OpCode["adv"] = 0] = "adv";
+    OpCode[OpCode["bxl"] = 1] = "bxl";
+    OpCode[OpCode["bst"] = 2] = "bst";
+    OpCode[OpCode["jnz"] = 3] = "jnz";
+    OpCode[OpCode["bxc"] = 4] = "bxc";
+    OpCode[OpCode["out"] = 5] = "out";
+    OpCode[OpCode["bdv"] = 6] = "bdv";
+    OpCode[OpCode["cdv"] = 7] = "cdv";
+})(OpCode || (OpCode = {}));
+function resolveCombo(comboOperand, registers) {
+    if (comboOperand <= 3) {
+        return BigInt(comboOperand);
+    }
+    if (comboOperand === 4) {
+        return registers.registerA;
+    }
+    if (comboOperand === 5) {
+        return registers.registerB;
+    }
+    if (comboOperand === 6) {
+        return registers.registerC;
+    }
+    if (comboOperand === 7) {
+        console.error("The text says, there is no comboOperand 7, but there is!");
+        throw new Error("The text says, there is no comboOperand 7, but there is!");
+    }
+    throw new Error("Is it a 3 bit program or not!?");
+}
+class ADVInstruction {
+    execute(operand, registers) {
+        const numerator = registers.registerA;
+        registers.registerA = (numerator / (1n << resolveCombo(operand, registers)));
+        return undefined;
+    }
+}
+class BXLInstruction {
+    execute(operand, registers) {
+        registers.registerB = registers.registerB ^ BigInt(operand);
+        return undefined;
+    }
+}
+class BSTInstruction {
+    execute(operand, registers) {
+        registers.registerB = resolveCombo(operand, registers) % 8n;
+        return undefined;
+    }
+}
+class JNZInstruction {
+    execute(operand, registers) {
+        if (registers.registerA === 0n) {
+            return undefined;
+        }
+        return operand;
+    }
+}
+class BXCInstruction {
+    execute(operand, registers) {
+        registers.registerB = registers.registerB ^ registers.registerC;
+        return undefined;
+    }
+}
+class OUTInstruction {
+    execute(operand, registers, output) {
+        output.value = resolveCombo(operand, registers) % 8n;
+        return undefined;
+    }
+}
+class BDVInstruction {
+    execute(operand, registers) {
+        const numerator = registers.registerA;
+        registers.registerB = (numerator / (1n << resolveCombo(operand, registers)));
+        return undefined;
+    }
+}
+class CDVInstruction {
+    execute(operand, registers) {
+        const numerator = registers.registerA;
+        registers.registerC = (numerator / (1n << resolveCombo(operand, registers)));
+        return undefined;
+    }
+}
+const cpuInstructions = [
+    new ADVInstruction(),
+    new BXLInstruction(),
+    new BSTInstruction(),
+    new JNZInstruction(),
+    new BXCInstruction(),
+    new OUTInstruction(),
+    new BDVInstruction(),
+    new CDVInstruction()
+];
+const registers = {
+    registerA: 0n,
+    registerB: 0n,
+    registerC: 0n
+};
+let program = [];
 const timer = "Execution time";
 console.time(timer);
 line_reader_1.default.eachLine("./input/input.txt", (line, last) => {
+    const registerAMatch = /^Register A: (\d+)$/.exec(line);
+    const registerBMatch = /^Register B: (\d+)$/.exec(line);
+    const registerCMatch = /^Register C: (\d+)$/.exec(line);
+    const programMatch = /^Program: (.+)$/.exec(line);
+    if (registerAMatch !== null) {
+        registers.registerA = BigInt(parseInt(registerAMatch[1]));
+    }
+    if (registerBMatch !== null) {
+        registers.registerB = BigInt(parseInt(registerBMatch[1]));
+    }
+    if (registerCMatch !== null) {
+        registers.registerC = BigInt(parseInt(registerCMatch[1]));
+    }
+    if (programMatch !== null) {
+        program = programMatch[1].split(",").map((instruction) => parseInt(instruction));
+    }
     if (last) {
+        let output = "";
+        let instructionPointer = 0;
+        while (instructionPointer < program.length) {
+            const instruction = program[instructionPointer];
+            const operand = program[instructionPointer + 1];
+            const opOutput = {
+                value: null
+            };
+            const jumpResult = cpuInstructions[instruction].execute(operand, registers, opOutput);
+            if (opOutput.value !== null) {
+                output += "" + opOutput.value + ",";
+            }
+            if (jumpResult !== undefined) {
+                instructionPointer = jumpResult;
+            }
+            else {
+                instructionPointer += 2;
+            }
+        }
+        output = output.substring(0, output.length - 1);
+        console.log(`Program output is: ${output}`);
         console.timeEnd(timer);
     }
 });
--- day17/src/puzzle01.ts
+++ day17/src/puzzle01.ts
@@ -2,10 +2,191 @@ console.log("Day 17, Puzzle 01!")
 
 import lineReader from "line-reader";
 
+enum OpCode {
+    adv,
+    bxl,
+    bst,
+    jnz,
+    bxc,
+    out,
+    bdv,
+    cdv
+}
+
+interface RawInstruction {
+    opCode: OpCode;
+    operand: number;
+}
+
+interface Registers {
+    registerA: bigint;
+    registerB: bigint;
+    registerC: bigint;
+}
+
+interface Output {
+    value: bigint | null;
+}
+
+interface Instruction {
+    // returns the new instruction pointer value (in case operation jumps) or undefined
+    execute: (operand: number, registers: Registers, output: Output) => number | undefined;
+}
+
+function resolveCombo(comboOperand: number, registers: Registers): bigint {
+    if (comboOperand <= 3) {
+        return BigInt(comboOperand);
+    }
+
+    if (comboOperand === 4) {
+        return registers.registerA;
+    }
+
+    if (comboOperand === 5) {
+        return registers.registerB;
+    }
+
+    if (comboOperand === 6) {
+        return registers.registerC;
+    }
+
+    if (comboOperand === 7) {
+        console.error("The text says, there is no comboOperand 7, but there is!");
+        throw new Error("The text says, there is no comboOperand 7, but there is!");
+    }
+
+    throw new Error("Is it a 3 bit program or not!?");
+}
+
+class ADVInstruction implements Instruction {
+    execute(operand: number, registers: Registers): number | undefined {
+        const numerator = registers.registerA;
+        registers.registerA = (numerator / (1n << resolveCombo(operand, registers)));
+        return undefined;
+    }
+}
+
+class BXLInstruction implements Instruction {
+    execute(operand: number, registers: Registers): number | undefined {
+        registers.registerB = registers.registerB ^ BigInt(operand);
+        return undefined;
+    }
+}
+
+class BSTInstruction implements Instruction {
+    execute(operand: number, registers: Registers): number | undefined {
+        registers.registerB = resolveCombo(operand, registers) % 8n;
+        return undefined;
+    }
+}
+
+class JNZInstruction implements Instruction {
+    execute(operand: number, registers: Registers): number | undefined {
+        if (registers.registerA === 0n) {
+            return undefined;
+        }
+
+        return operand;
+    }
+}
+
+class BXCInstruction implements Instruction {
+    execute(operand: number, registers: Registers): number | undefined {
+        registers.registerB = registers.registerB ^ registers.registerC;
+        return undefined;
+    }
+}
+
+class OUTInstruction implements Instruction {
+    execute(operand: number, registers: Registers, output: Output): number | undefined {
+        output.value = resolveCombo(operand, registers) % 8n;
+        return undefined;
+    }
+}
+
+class BDVInstruction implements Instruction {
+    execute(operand: number, registers: Registers): number | undefined {
+        const numerator = registers.registerA;
+        registers.registerB = (numerator / (1n << resolveCombo(operand, registers)));
+        return undefined;
+    }
+}
+
+class CDVInstruction implements Instruction {
+    execute(operand: number, registers: Registers): number | undefined {
+        const numerator = registers.registerA;
+        registers.registerC = (numerator / (1n << resolveCombo(operand, registers)));
+        return undefined;
+    }
+}
+
+const cpuInstructions: Instruction[] = [
+    new ADVInstruction(),
+    new BXLInstruction(),
+    new BSTInstruction(),
+    new JNZInstruction(),
+    new BXCInstruction(),
+    new OUTInstruction(),
+    new BDVInstruction(),
+    new CDVInstruction()
+];
+
+const registers: Registers = {
+    registerA: 0n,
+    registerB: 0n,
+    registerC: 0n
+}
+
+let program: number[] = [];
+
 const timer = "Execution time";
 console.time(timer);
 lineReader.eachLine("./input/input.txt", (line, last) => {
+    const registerAMatch = /^Register A: (\d+)$/.exec(line);
+    const registerBMatch = /^Register B: (\d+)$/.exec(line);
+    const registerCMatch = /^Register C: (\d+)$/.exec(line);
+    const programMatch = /^Program: (.+)$/.exec(line);
+
+    if (registerAMatch !== null) {
+        registers.registerA = BigInt(parseInt(registerAMatch[1]));
+    }
+
+    if (registerBMatch !== null) {
+        registers.registerB = BigInt(parseInt(registerBMatch[1]));
+    }
+
+    if (registerCMatch !== null) {
+        registers.registerC = BigInt(parseInt(registerCMatch[1]));
+    }
+
+    if (programMatch !== null) {
+        program = programMatch[1].split(",").map((instruction) => parseInt(instruction));
+    }
+
     if (last) {
+        let output: string = "";
+        let instructionPointer = 0;
+        while (instructionPointer < program.length) {
+            const instruction = program[instructionPointer];
+            const operand = program[instructionPointer + 1];
+            const opOutput: Output = {
+                value: null
+            }
+            const jumpResult = cpuInstructions[instruction].execute(operand, registers, opOutput);
+            if (opOutput.value !== null) {
+                output += "" + opOutput.value + ",";
+            }
+
+            if (jumpResult !== undefined) {
+                instructionPointer = jumpResult;
+            } else {
+                instructionPointer += 2;
+            }
+        }
+
+        output = output.substring(0, output.length - 1);
+        console.log(`Program output is: ${output}`);
+
         console.timeEnd(timer);
     }
 });
