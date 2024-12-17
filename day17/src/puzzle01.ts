20654d48521ba591339492236c58657274c3e512
Kay Huber <kay.huber@unblu.com>
2024-12-17 07:19:49 +0100

    Day17 start

---
+++ day17/src/puzzle01.ts
@@ -0,0 +1,11 @@
+console.log("Day 17, Puzzle 01!")
+
+import lineReader from "line-reader";
+
+const timer = "Execution time";
+console.time(timer);
+lineReader.eachLine("./input/input.txt", (line, last) => {
+    if (last) {
+        console.timeEnd(timer);
+    }
+});
